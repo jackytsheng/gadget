39080610c4e7acbd5275323d72a945697bc98436
jackytsheng <jackytsheng@gmail.com>
2020-08-13 23:20:14 +1000

    Updates

---
+++ precache-manifest.61fee13b197a540bb526d5a04bd4d76f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0437777fbd486712647658baf5cc1513",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6939c465ac37434d6e7c",
+    "url": "./static/css/main.e005791f.chunk.css"
+  },
+  {
+    "revision": "9597e52920d1ea7e47cd",
+    "url": "./static/js/2.e3aa65ef.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.e3aa65ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6939c465ac37434d6e7c",
+    "url": "./static/js/main.b1ce0c62.chunk.js"
+  },
+  {
+    "revision": "58799c2a76c51559c70e",
+    "url": "./static/js/runtime-main.4d770a51.js"
+  }
+]);
