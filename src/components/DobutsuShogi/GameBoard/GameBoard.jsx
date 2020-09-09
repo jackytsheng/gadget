c1f13bd200471df72b4ddfc13a62f6178f87bb3f
jackytsheng <jackytsheng@gmail.com>
2020-09-10 00:35:35 +1000

    updated the background image

--- src/components/DobutsuShogi/GameBoard/GameBoard.jsx
+++ src/components/DobutsuShogi/GameBoard/GameBoard.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 import CenterWrapper from '../../../Layout/CenterWrapper';
-import BoardSvg from './BoardSVG.svg';
+import BackgroundImg from "./background.png";
+
+const BOARD_HEIGHT = '540px';
+const BOARD_WIDTH = "380px";
+const DASH_COLOR = "#b52838";
 const Board = styled.div`
-  width: 380px;
-  height: 540px;
+  width: ${BOARD_WIDTH};
+  height: ${BOARD_HEIGHT};
   border-radius: 5px;
   border: 1px solid black;
+  overflow:hidden;
+  position:relative;
 `;
 
+const Img = styled.img`
+  width: ${BOARD_WIDTH};
+  height: ${BOARD_HEIGHT};
+  position:absolute;
+`;
 const GridSquare = styled.div`
   width: ${(props) => (props.rightLast ? "100px" : "99px")};
   height: ${(props) => (props.bottomLast ? "100px" : "99px")};
-  border-bottom: ${(props) => (props.bottomLast ? null : "1px dashed red")};
-  border-right: ${(props) => (props.rightLast ? null : "1px dashed red")};
+  border-bottom: ${(props) =>
+    props.bottomLast ? null : "1px dashed " + DASH_COLOR};
+  border-right: ${(props) =>
+    props.rightLast ? null : "1px dashed" + DASH_COLOR};
 `;
 const Grid = styled.div`
   width: 300px;
@@ -21,14 +34,14 @@ const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: repeat(4, 1fr);
-  border: 1px dashed red;
+  border: 1px dashed ${DASH_COLOR};
+  z-index: 2;
 `;
 
 export default () => (
   <Board>
-    <BoardSvg></BoardSvg>
+    <Img src={BackgroundImg} alt={"background"} />
     <CenterWrapper>
-
       <Grid>
         <GridSquare />
         <GridSquare />
