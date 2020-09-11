78b14ad77934c875d3a965fae237e7a6dc09f243
jackytsheng <jackytsheng@gmail.com>
2020-09-11 16:51:27 +1000

    update on UI

--- src/components/DobutsuShogi/Chess/Chess.jsx
+++ src/components/DobutsuShogi/Chess/Chess.jsx
@@ -5,23 +5,30 @@ import ChessLayout from './components/ChessLayout';
 
 const CHESS_BORDER = "#f5e379";
 const CHESS_BORDER_BLACK = "#272016";
-const SELECT_BORDER_BG = "#15cd3185";
+const SELECT_BORDER_BG_P1 = "#15cd3185";
+const SELECT_BORDER_BG_P2 = "#cd151585";
 const CHESS_SIZE = 80;
 const ChessWrapper = styled.div`
-  :hover {
-    cursor: pointer;
+  &:hover {
+    cursor: ${(props) => (props.disable ? "auto" : "pointer")};
+    ${(props) =>
+      props.disable
+        ? ""
+        : "box-shadow: 0 3px 15px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)"};
   }
   width: ${CHESS_SIZE - 20}px;
   height: ${CHESS_SIZE - 20}px;
   border-radius: 10px;
-  background: ${(props) => (props.selected ? SELECT_BORDER_BG : CHESS_BORDER)};
+  background: ${(props) =>
+    props.selected
+      ? props.player === 1
+        ? SELECT_BORDER_BG_P1
+        : SELECT_BORDER_BG_P2
+      : CHESS_BORDER};
   box-shadow: 5px 1px 10px 3px rgba(0, 0, 0, 0.12),
     0 1px 2px rgba(0, 0, 0, 0.24);
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
   padding: 10px;
-  &:hover {
-    box-shadow: 0 3px 15px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-  }
 `;
 const InnerWrapper = styled.div`
   transform: ${(props) => (props.rotated ? "rotateZ(180deg)" : null)};
@@ -33,8 +40,8 @@ const InnerWrapper = styled.div`
   overflow: hidden;
 `;
 
-const Chess = ({ chessType, rotated, onClick, selected }) => (
-  <ChessWrapper onClick={onClick} selected={selected}>
+const Chess = ({ chessType, rotated, onClick, selected, player, disable }) => (
+  <ChessWrapper onClick={onClick} selected={selected} player={player} disable={disable}>
     <CenterWrapper>
       <InnerWrapper rotated={rotated}>
         <ChessLayout chessType={chessType} />
--- src/components/DobutsuShogi/GameBoard/GameBoard.jsx
+++ src/components/DobutsuShogi/GameBoard/GameBoard.jsx
@@ -4,7 +4,6 @@ import CenterWrapper from "../../../Layout/CenterWrapper";
 import BackgroundImg from "./background.png";
 import Chess from "../Chess";
 
-
 const BOARD_HEIGHT = "540px";
 const BOARD_WIDTH = "380px";
 const DASH_COLOR = "#b52838";
@@ -25,6 +24,7 @@ const Img = styled.img`
 const GridSquare = styled.div`
   width: ${(props) => (props.rightLast ? "100px" : "99px")};
   height: ${(props) => (props.bottomLast ? "100px" : "99px")};
+  position:relative;
   border-bottom: ${(props) =>
     props.bottomLast ? null : "1px dashed " + DASH_COLOR};
   border-right: ${(props) =>
@@ -38,6 +38,44 @@ const Grid = styled.div`
   grid-template-rows: repeat(4, 1fr);
   border: 1px dashed ${DASH_COLOR};
   z-index: 2;
+`;
+
+const EmptySpace = styled.div`
+  :hover{
+    cursor:${props => props.isMovable ? "pointer" : "auto"};
+  }
+  position:absolute;
+  top:0;
+  left:0;
+  bottom:0;
+  right:0;
+`
+
+const CapturedChessWrapper = styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  width:100px;
+  height:100px;
+`;
+const CapturePool = styled.div`
+  width: 305px;
+  height: 305px;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ReverseCapturePool = styled(CapturePool)`
+  flex-direction:row-reverse;
+`
+const BaseStyleCapture = styled.div`
+  height: ${BOARD_HEIGHT};
+  display:flex;
+  flex-direction:column;
+`;
+const SecondPlayerCapture = styled(BaseStyleCapture)``;
+const FirstPlayerCapture = styled(BaseStyleCapture)`
+  flex-direction: column-reverse;
 `;
 
 
@@ -69,49 +107,282 @@ const gameBoard = [
   ["E", "L", "G"],
 ];
 
-class GameBoard extends React.Component{
-  constructor(props){
+class GameBoard extends React.Component {
+  constructor(props) {
     super(props);
 
     this.state = {
       gameBoard: JSON.parse(JSON.stringify(gameBoard)),
       selectedChess: null,
-    }
+      player: 1,
+      playerOneCapture: [],
+      playerTwoCapture: [],
+      selectedCaptureID:"",
+      selectedCoor: {row:null,column:null},
+    };
     this.handleClickChess = this.handleClickChess.bind(this);
+    this.handleClickCapture = this.handleClickCapture.bind(this);
   }
-  generateChess(board){
-    return board.map((row, i) =>
-    row.map((chessCode, j) => (
-      <GridSquare
-        key={"Square" + chessCode + i + j}
-        rightLast={j === 2}
-        bottomLast={i === 3}
-      >
-        {chessCode ? (
-          <Chess
-            selected = {this.state.selectedChess === chessCode}
-            onClick = {()=> this.handleClickChess(chessCode)}
-            key={"Chess" + chessCode + i + j}
-            chessType={renderType(chessCode)}
-            rotated={chessCode < "z" && chessCode > "a"}
-          />
-        ) : null}
-      </GridSquare>
-    ))
-  );
-}
-  handleClickChess(chessCode){
-    this.setState({selectedChess:chessCode});
+  isPlayerTwo(chessCode) {
+    return chessCode < "z" && chessCode > "a";
   }
-  render(){
-    return (<Board>
-      <Img src={BackgroundImg} alt={"background"} />
-      <CenterWrapper>
-        <Grid>{this.generateChess(this.state.gameBoard)}</Grid>
-      </CenterWrapper>
-    </Board>)
+  generateCapture(player) {
+    const {playerOneCapture,playerTwoCapture} = this.state;
+    let array = player === 1 ? playerOneCapture: playerTwoCapture;
+    return array.map((chessCode) => (
+      <CapturedChessWrapper key={"CaptureWrapper" + chessCode + Math.random()}>
+        <Chess
+          onClick={() => this.handleClickCapture(chessCode)}
+          disable={this.state.player !== player}
+          player={this.state.player}
+          selected={this.state.selectedCaptureID === chessCode}
+          key={"Capture" + chessCode + Math.random()}
+          chessType={renderType(chessCode[0])}
+          rotated={this.isPlayerTwo(chessCode[0])}
+        />
+      </CapturedChessWrapper>
+    ));
+    
+  }
+  generateChess() {
+    return this.state.gameBoard.map((row, i) =>
+      row.map((chessCode, j) => (
+        <GridSquare
+          key={"Square" + chessCode + i + j}
+          rightLast={j === 2}
+          bottomLast={i === 3}
+        >
+          {chessCode ? (
+            <Chess
+              disable = {!this.isPickable(chessCode) && !this.isMovable(
+                this.state.selectedChess,this.state.selectedCoor,{ row: i, column: j })}
+
+              player = {this.state.player}
+              selected={this.state.selectedChess === chessCode && !this.state.selectedCaptureID}
+              onClick={() =>
+                this.handleClickChess(chessCode, { row: i, column: j })
+              }
+              key={"Chess" + chessCode + i + j}
+              chessType={renderType(chessCode)}
+              rotated={this.isPlayerTwo(chessCode)}
+            />
+          ) : (
+            <EmptySpace
+              isMovable={this.isMovable(
+                this.state.selectedChess,
+                this.state.selectedCoor,
+                { row: i, column: j }
+              ) || this.state.selectedCaptureID}
+              onClick={() =>
+                this.handleClickChess(chessCode, { row: i, column: j })
+              }
+            ></EmptySpace>
+          )}
+        </GridSquare>
+      ))
+    );
+  }
+
+  captureChess(coor){
+    const {gameBoard} = this.state;
+    let chessCode = gameBoard[coor.row][coor.column];
+    let { player, playerOneCapture,playerTwoCapture} = this.state;
+    playerOneCapture = JSON.parse(JSON.stringify(playerOneCapture));
+    playerTwoCapture = JSON.parse(JSON.stringify(playerTwoCapture));
+    // TODO: Account also for "HEN" chess
+    if(player === 1){
+      playerOneCapture.push(
+        playerOneCapture.includes(chessCode.toUpperCase())
+          ? chessCode.toUpperCase() + 2
+          : chessCode.toUpperCase()
+      );
+      this.setState({ playerOneCapture }, () => console.log(playerOneCapture));
+    }else{
+      playerTwoCapture.push(
+        playerTwoCapture.includes(chessCode.toLowerCase())
+          ? chessCode.toLowerCase() + 2
+          : chessCode.toLowerCase()
+      );
+      this.setState({ playerTwoCapture },()=> console.log(playerTwoCapture));
+    }
   }
   
+  swapPlayer(){
+    const {player} = this.state;
+    this.setState({player:player === 1? 2:1});
+  }
+  isPickable(chessCode){
+    const { player } = this.state;
+    return (
+      (this.isPlayerTwo(chessCode) && player === 2) ||
+      (!this.isPlayerTwo(chessCode) && player === 1)
+    );
+  }
+  isMovable(selectedChess, oldCoor, newCoor){
+    let totalDistance;
+    const { gameBoard } = this.state;
+
+    // Check if it's alias's chess
+    if (
+      this.isPickable(gameBoard[newCoor.row][newCoor.column]) &&
+      gameBoard[newCoor.row][newCoor.column] !== 0
+    ) {
+      return false;
+    }
+
+    switch (selectedChess) {
+      case "L":
+      case "l":
+        return (
+          Math.abs(oldCoor.row - newCoor.row) <= 1 &&
+          Math.abs(oldCoor.column - newCoor.column) <= 1
+        );
+      case "g":
+      case "G":
+        totalDistance =
+          Math.abs(oldCoor.column - newCoor.column) +
+          Math.abs(oldCoor.row - newCoor.row);
+        return totalDistance === 1;
+      case "e":
+      case "E":
+        totalDistance =
+          Math.abs(oldCoor.column - newCoor.column) +
+          Math.abs(oldCoor.row - newCoor.row);
+        return totalDistance === 2;
+      case "c":
+        return newCoor.row - oldCoor.row === 1;
+      case "C":
+        return oldCoor.row - newCoor.row === 1;
+      default:
+        return false;
+    }
+    
+  }
+  move(oldCoor, newCoor){
+    const {gameBoard} = this.state;
+    let newGameBoard = JSON.parse(JSON.stringify(gameBoard));
+    console.log("Moving");
+    this.setState({
+      selectedChess: null,
+      selectedCoor: { row: null, column: null },
+    });
+
+    if(newGameBoard[newCoor.row][newCoor.column] !== 0){
+      this.captureChess(newCoor);
+    }
+      newGameBoard[newCoor.row][newCoor.column] =
+        newGameBoard[oldCoor.row][oldCoor.column];
+      newGameBoard[oldCoor.row][oldCoor.column] = 0;
+      this.setState({gameBoard: JSON.parse(JSON.stringify(newGameBoard))},()=>console.log(this.state.gameBoard))
+  }
+  handleClickCapture(chessCode){
+    const { selectedCaptureID } = this.state;
+    if (selectedCaptureID === chessCode) {
+      // Put it down
+      this.setState({ selectedCaptureID:"" });
+    } else if (this.isPickable(chessCode[0])){
+      
+      // Pick it up
+      this.setState(
+        {
+          selectedCaptureID: chessCode,
+          selectedChess: chessCode[0],
+          selectedCoor: { row: null, column: null },
+        },
+        () => console.log(this.state.selectedCaptureID)
+      );
+    }
+  }
+  isPlacable(coor){
+    const { gameBoard } = this.state;
+    return !gameBoard[coor.row][coor.column];
+  }
+
+  place(coor){
+    const {gameBoard,selectedCaptureID,selectedChess,playerOneCapture,playerTwoCapture} = this.state;
+    let newGameBoard = JSON.parse(JSON.stringify(gameBoard));
+    let newPlayerOneCapture = [...playerOneCapture];
+    let newPlayerTwoCapture = [...playerTwoCapture];
+
+    console.log("Placing");
+
+    // place the chess on the board
+    newGameBoard[coor.row][coor.column] = selectedChess;
+
+    // update capture pool
+    newPlayerOneCapture = playerOneCapture.filter(
+      (chessCode) => chessCode !== selectedCaptureID
+    );
+
+    newPlayerTwoCapture = playerTwoCapture.filter(
+      (chessCode) => chessCode !== selectedCaptureID
+    );
+
+    this.setState({
+      selectedCaptureID: "",
+      selectedChess: null,
+      selectedCoor: { row: null, column: null },
+      playerOneCapture:newPlayerOneCapture,
+      playerTwoCapture:newPlayerTwoCapture,
+      gameBoard:JSON.parse(JSON.stringify(newGameBoard))
+    },()=> console.log(this.state.gameBoard));
+    
+  }
+
+  handleClickChess(chessCode,newCoordinate) {
+
+    const { selectedChess, selectedCoor,selectedCaptureID } = this.state;
+    if (selectedChess === chessCode && !selectedCaptureID) {
+      // put it down
+      this.setState({
+        selectedChess: null,
+        selectedCoor: { row: null, column: null },
+      });
+
+    } else if (chessCode === selectedCaptureID) {
+      // put it down from capture and pick up from board
+      this.setState({selectedCaptureID:"", selectedChess: chessCode, selectedCoor: newCoordinate })
+
+    }else if (chessCode && this.isPickable(chessCode)) {
+      // pick it up
+      this.setState(
+        { selectedChess: chessCode, selectedCoor: newCoordinate },
+        () => console.log(this.state.selectedChess)
+      );
+    } else if (selectedCaptureID && this.isPlacable(newCoordinate)) {
+      
+      // place the chess
+      this.place(newCoordinate);
+
+      // swapPlayer
+      this.swapPlayer();
+
+    } else if (this.isMovable(selectedChess, selectedCoor, newCoordinate)) {
+      // Move to a new place
+      this.move(selectedCoor, newCoordinate);
+
+      // swapPlayer
+      this.swapPlayer();
+    } 
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <SecondPlayerCapture>
+          <ReverseCapturePool>{this.generateCapture(2)}</ReverseCapturePool>
+        </SecondPlayerCapture>
+        <Board>
+          <Img src={BackgroundImg} alt={"background"} />
+          <CenterWrapper>
+            <Grid>{this.generateChess()}</Grid>
+          </CenterWrapper>
+        </Board>
+        <FirstPlayerCapture>
+          <CapturePool>{this.generateCapture(1)}</CapturePool>
+        </FirstPlayerCapture>
+      </React.Fragment>
+    );
+  }
 }
 
 export default GameBoard;
