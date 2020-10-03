6daf9d5f93e0620898db9cf6e1502350c7b99b6e
jackytsheng <jackytsheng@gmail.com>
2020-10-03 17:13:55 +1000

    changes

--- src/components/Siege/components/Fence.jsx
+++ src/components/Siege/components/Fence.jsx
@@ -3,7 +3,18 @@ import styled from "styled-components";
 import PlayerOneFence from "../svg/PlayerOneFence.svg";
 
 const UpFence = styled.div``;
-const FenceBase = styled.div``;
+
+
+const FenceBase = styled.div`
+  position: relative;
+  top: 85%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 31%;
+  
+`;
 const FenceWrapper = styled.div`
   position: absolute;
   top: 0;
@@ -12,9 +23,7 @@ const FenceWrapper = styled.div`
   bottom: 0;
 `;
 const FenceImg = styled.img`
-  position: relative;
-  top: 50%;
-  width: 100%;
+  width:100%;
 `;
 
 // const LeftFence =
@@ -23,7 +32,9 @@ const FenceImg = styled.img`
 
 const Fence = ({ onClick, player, fenceOption, confirmed }) => (
   <FenceWrapper>
-    <FenceImg src={PlayerOneFence} alt={"Player one fence"}></FenceImg>
+    <FenceBase>
+      <FenceImg src={PlayerOneFence} alt={"Player one fence"}></FenceImg>
+    </FenceBase>
   </FenceWrapper>
 );
 
